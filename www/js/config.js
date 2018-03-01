17bfcd4c5bf7874197e89bc6466d9be12fb64dc8
Jamie McClelland <jm@mayfirst.org>
2018-03-01 15:19:40 -0500

    provide sample config so users can modify

--- www/js/config.js
+++
@@ -1,6 +0,0 @@
-// Set site specific configuration variables here.
-// See js/icecream.js for full list of options.
-iConfig = {
-  iceServerDomain: 'a.stream.mayfirst.org',
-  watchUrl: 'https://a.stream.mayfirst.org/',
-};
