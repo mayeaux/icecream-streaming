3a66a298a8b407e1c8bb759d4d61b9b9b65af28e
Jamie McClelland <jm@mayfirst.org>
2018-03-02 09:15:40 -0500

    futher refactoring to make it easier to customize

--- www/js/icecream.js
+++ www/js/icecream.js
@@ -2,7 +2,10 @@
 function Icecream(config) {
   var pageTitle = config['pageTitle'] || 'Icecream Icecast Video Source Client';
   var logoPath = config['logoPath'] || '/images/icecream-small.png';
+  var footer = config['footer'] || '<strong>Icecream</strong> is free software. Browse the <strong>icecream</strong> source code via <a href="https://gitlab.com/jamie/icecream">gitlab</a>.';
 
   document.querySelector('#titleHeader').innerHTML = pageTitle;
   document.querySelector('#logo').src = logoPath;
+  document.querySelector('#footer').innerHTML = footer;
+  
 }
